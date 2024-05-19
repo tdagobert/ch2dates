5f0f111e935f1a7956f8bc4b08690cbd50b64ff2
dagobert <tristan.dagobert@ens-paris-saclay.fr>
2024-05-19 15:38:40 +0200

    rectif listage zip

--- kervrann.py
+++ kervrann.py
@@ -608,10 +608,14 @@ def calculer_triplet_dates(cfg):
     """
 
     # liste ordonnées des fichiers contenus dans le zip
-    repzip = cfg.repout + "_zip"
+    repzip = join(cfg.repout, "zip")
+    if not exists(repzip):
+        os.mkdir(repzip)
+    print("répertoire existe :", exists(repzip))
+
     with zipfile.ZipFile(cfg.zip, 'r') as monzip:
         fichiers = sorted([basename(f) for f in monzip.namelist()])
-        monzip.extractall(repzip)
+        monzip.extractall(path=repzip)
 
 #    fichiers = sorted(os.listdir(repzip))
     print(fichiers)
